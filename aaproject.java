d35250aa65ecaaf5440407895dfa679ec4ba61e2
odobo3581 <janssonks@gmail.com>
2023-07-09 15:26:14 -0500

    Add files via upload

---
+++ aaproject.java
@@ -0,0 +1,16 @@
+package aaproject;
+
+public class aaproject {
+
+	public static void main(String[] args) {
+		for(int i = 1; i<=1000; i++) {
+			System.out.print(i*i + " ");
+			System.out.print(" " + i+i + " \n");
+			System.out.print(" " + i+i + " \n");
+					}
+		
+		
+		
+	}
+
+}
